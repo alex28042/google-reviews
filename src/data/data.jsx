fe87365e6d3b894ccac39c1d602e0efd163b9a78
alex <alex28042@gmail.com>
2023-03-15 19:29:33 +0100

    added dropdown menu

---
+++ src/data/data.jsx
@@ -0,0 +1,7 @@
+const reviewsData = [
+  { id: 1, title: "1 Google Review 5 stars" },
+  { id: 2, title: "5 Google Review 5 stars" },
+  { id: 3, title: "10 Google Review 5 stars" },
+  { id: 4, title: "15 Google Review 5 stars" },
+  { id: 5, title: "20 Google Review 5 stars" },
+];
